44c08a97a6770bbe572b8ba8e1b6d5baf66a81ae
Kenza <a.kenza29@gmail.com>
2024-05-17 16:56:54 +0200

    first commit

---
+++ partie1/exo1.php
@@ -0,0 +1,48 @@
+<h1> Exercice 1</h1>
+
+<p>Soit la phrase « Notre formation DL commence aujourd'hui ».
+Ecrire un algorithme permettant de compter le nombre de caractères contenus dans cette phrase 
+(espaces inclus).
+</p>
+
+<h2>Résultat</h2>
+
+
+<?php
+
+$phrase = "Notre formation DL commence aujourd'hui";
+$nbLongueur = strlen($phrase);
+
+echo "La phrase «$phrase » contien $nbLongueur caractères <br>";
+echo "La phrase « $phrase » contient". strlen($phrase). " caractères <br>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ partie1/exo3.php
@@ -0,0 +1,23 @@
+
+<h1>Exercice 3</h1>
+
+<p>A partir de la phrase de l’exercice 1, écrire l’instruction permettant de remplacer le mot 
+« aujourd’hui » par le mot « demain ». Afficher l’ancienne et la nouvelle phrase.</p>
+
+
+<h2>Résultat</h2>
+
+
+<?php
+
+$phrase = " « Notre formation DL commence aujourd’hui »";
+$mots = array("aujourd'hui");
+$newMots = array("demain");
+$newPhrase = str_replace("aujourd'hui","demain",$phrase);
+
+echo $phrase. "<br>";
+echo $newPhrase;
+
+
+
+
